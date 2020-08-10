757399e6483520103a3cabaf6de000412d0cadf9
alexchung <yonganzhong@outlook.com>
2020-08-10 17:05:42 +0800

    add ssd image process

---
+++ data/preprocessing/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : __init__.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/8/5 下午2:39
+# @ Software   : PyCharm
+#-------------------------------------------------------
